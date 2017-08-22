1e8bff097002a22028aaefe841f8db340c23b214
Lukas Pavez <lukas.pavez@ug.uchile.cl>
2017-08-22 15:49:33 -0300

    fix insertar_bits

--- Primavera_2017/T1/t1.c
+++ Primavera_2017/T1/t1.c
@@ -5,9 +5,28 @@
 #include "t1.h"
 
 uint insertar_bits(uint x, int pos, uint y, int len) {
-	uint mask = 0xffffffff;
-	printf("%d\n", mask & 0xf);
-	return mask;
+	int res  = 0;
+	int maskx = 1;
+	int masky = 1;
+	int i = 31 - len;
+	int j = 0;
+	int aux = 0;
+	while(i>=j){
+		if(j==pos){
+			while(len > aux){
+				if(masky & y)
+					res = res | masky;
+				masky = masky << 1;
+				aux++;
+			}
+		}
+		if(maskx & x)
+			res = res | masky;
+		maskx = maskx << 1;
+		masky = masky << 1;
+		j++;
+	}
+	return res;
 
 }
 
@@ -16,8 +35,7 @@ void eliminar_espacios(char *s) {
 	printf("%s\n", "\n");
 }
 
-int main(int argc, char const *argv[])
-{
+/*int main(int argc, char const *argv[]) {
 	insertar_bits(0x2, 0, 1, 1);
 	return 0;
-}
+}*/
