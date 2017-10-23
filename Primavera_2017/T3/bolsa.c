e0b238430aeb9750461b6169036e96777434e300
Lukas-Pavez <lukas.pavez@ug.uchile.cl>
2017-10-23 00:58:57 -0300

    no pasa el 2 test

--- Primavera_2017/T3/bolsa.c
+++ Primavera_2017/T3/bolsa.c
@@ -5,13 +5,11 @@
 
 #include "bolsa.h"
 
-pthread_mutex_t buyer_mutex;
 pthread_mutex_t m;
-pthread_mutex_t s;
 pthread_cond_t buying;
 
+int num = 0;
 int m_precio = 0;
-int price = 0;
 char* seller;
 char* buyer;
 
@@ -33,19 +31,25 @@ int vendo(int precio, char *vendedor, char *comprador){
 			return 0;
 		}
 		copy(buyer, comprador);
+		m_precio = 0;
+		buyer = NULL;
+		seller = NULL;
 		pthread_mutex_unlock(&m);
 		return 1;
 	}
 	else if(m_precio > precio){
 		m_precio = precio;
-		pthread_cond_broadcast(&buying);
 		seller = vendedor;
+		pthread_cond_broadcast(&buying);
 		pthread_cond_wait(&buying, &m);
 		if(m_precio < precio){
 			pthread_mutex_unlock(&m);
 			return 0;
 		}
 		copy(buyer, comprador);
+		buyer = NULL;
+		seller = NULL;
+		m_precio = 0;
 		pthread_mutex_unlock(&m);
 		return 1;
 	}
@@ -56,13 +60,15 @@ int vendo(int precio, char *vendedor, char *comprador){
 }
 
 int compro(char *comprador, char *vendedor){
-	pthread_mutex_lock(&buyer_mutex);
-	if(m_precio == 0)
+	pthread_mutex_lock(&m);
+	if(m_precio == 0){
+		pthread_mutex_unlock(&m);
 		return 0;
+	}
 	copy(seller,vendedor);
 	buyer = comprador;
 	pthread_cond_broadcast(&buying);
-	pthread_mutex_unlock(&buyer_mutex);
+	pthread_mutex_unlock(&m);
 	return m_precio;
 }
 
