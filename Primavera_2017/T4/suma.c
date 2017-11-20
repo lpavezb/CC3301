98f00f278b81a6a7b784464d2bf4b8ebb463669f
Lukas Pavez <lukas.pavez@ug.uchile.cl>
2017-11-20 17:30:12 -0300

    tarea funciona dependiendo de pid_t children[8]

--- Primavera_2017/T4/suma.c
+++ Primavera_2017/T4/suma.c
@@ -1,20 +1,33 @@
 #define _POSIX_C_SOURCE 1
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <unistd.h>
+
 #include "suma.h"
 
 #define BUFF_SIZE 8192
 
-pid_t children[8];
 
+int leer(int fd, void *buf, int n) {
+  if (n==0)
+    return 0;
+  do {
+    int rc= read(fd, buf, n);
+    if (rc<=0)
+      return 1; /* fracaso: error o fin del archivo/pipe/socket */
+    n-= rc; /* descontamos los bytes leídos */
+    buf= (char*)buf + rc; /* avanzamos el buffer */
+  } while (n>0); /* mientras no leamos todo lo que esperamos */
+  return 0; /* exito */
+}
 
 Set buscar(int a[], int n) {
+	pid_t children[8];
 	int fds[8][2];
 	Set comb= ((1<<(n-1)<<1)-1)/8;
+	Set res = 0;
 	for (int i = 0; i < 8; ++i)	{
 		int fd[2];
 		fds[i][0]=fd[0];
@@ -24,8 +37,8 @@ Set buscar(int a[], int n) {
 			exit(1);
 		}
 		if ((children[i]=fork())==0){
-			Set k   = (comb*i) + 1;
-			Set end = k + comb -1;
+			Set k   = comb*i +1;
+			Set end = k + comb;
 			for (; k<=end; k++) {
 				long long sum= 0;
 				for (int j= 0; j<n; j++) {
@@ -33,32 +46,43 @@ Set buscar(int a[], int n) {
 						sum+= a[j];
 				}
 				if (sum==0){
-					printf("child: %lld\n", k);
 					close(fds[i][0]);
-					write(fds[i][1],&k,sizeof(k));
+					write(fds[i][1],&k,sizeof(Set));
+					close(fds[i][1]);
 					exit(0);
 				}
 			}
 			exit(0);
 		}
+		else if(children[i]>0)
+			continue;
+		else
+			perror("fork error");
 	}
+
+	int break_aux = 0;
+
 	for (int i = 0; i < 8; i++){
-		pid_t pid = waitpid(children[i],NULL,0);
+		pid_t pid = waitpid(0,NULL,0);
 		for (int j = 0; j < 8; ++j){
 			if(children[j]==pid){
 				children[j] = -1;
-				Set res;
 				close(fds[j][1]);
-				read(fds[j][0],&res,sizeof(res));
-				if(res!=0 && res<1000000){
+				leer(fds[j][0],&res,sizeof(Set));
+				close(fds[i][0]);
+				if(res!=0){
+					break_aux = 1;
 					for (int k = 0; k < 8; k++){
 						if(children[k]!=-1)
 							kill(children[k], SIGTERM);
 					}
-					return res;
+					break;
 				}
 			}
 		}
+		if(break_aux){break;}
 	}
-	return 0;
+	for (int i = 0; i < 8; ++i)
+		waitpid(children[i],NULL,0);
+	return res;
 }
